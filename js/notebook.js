aabfcd91bea3eaf32bc35f5daf836cf9480f188d
Ingo Andelhofs <ingom2000@gmail.com>
2019-10-04 19:25:21 +0200

    Added support for math rendering on focusout.

--- js/notebook.js
+++ js/notebook.js
@@ -6,7 +6,13 @@ class Notebook {
         try {
             this.noteBook = document.getElementById(noteBookId);
             this.firstLineNote = this.noteBook.getElementsByTagName('span')[0];
+
             this.firstLineNote.addEventListener('keydown', this.actionEvent.bind(this));
+
+            this.firstLineNote.addEventListener('focus', this.focusEvent.bind(this));
+            this.firstLineNote.addEventListener('focusout', this.unfocusEvent.bind(this));
+
+
             this.firstLineNote.focus(); 
             console.log(`Notebook created.`);
         }
@@ -20,6 +26,8 @@ class Notebook {
         newNoteItem.setAttribute('contenteditable', 'true');
         newNoteItem.dataset.enterType = 'new-line';
         newNoteItem.dataset.type = '';
+        newNoteItem.dataset.plainData = '';
+        newNoteItem.dataset.mathSupport = true;
         return newNoteItem;
     }
 
@@ -60,25 +68,9 @@ class Notebook {
             }
         }
 
-        // Special elements
-        if (event.code === 'Space') {
-            this.setLineNoteTypeEvent(event, '#', 'header1');
-            this.setLineNoteTypeEvent(event, '##', 'header2');
-            this.setLineNoteTypeEvent(event, '###', 'header3');
-            
-            this.setLineNoteTypeEvent(event, '-', 'bullet-list', 'expand-line', `<ul><li></li></ul>`);
-            this.setLineNoteTypeEvent(event, '1.', 'number-list', 'expand-line', `<ol><li></li></ol>`);
-            this.setLineNoteTypeEvent(event, '-[]', 'selection-list', 'expand-line', `<ul><li><input type="checkbox">Select list not yet implemented</li></ul>`);
-            
-            this.setLineNoteTypeEvent(event, 'img', 'image', 'new-line', `<img src="https://via.placeholder.com/350x200" alt="test">`);
-            
-            
-            this.setLineNoteTypeEvent(event, '&gt;', 'quote', `Quote not yet implemented`); // >
-            this.setLineNoteTypeEvent(event, '&gt;&gt;&gt;', 'code', `Code not yet implemented`); // >>>
-            this.setLineNoteTypeEvent(event, '$$', 'math', `Math not yet implemented`);
-        }
+        this.supportedActionsEvent(event);
 
-
+        // TODO: fix if element is delete -> cant move to display:none
         // Moving
         if (event.code === 'ArrowDown') {
             try {
@@ -98,6 +90,95 @@ class Notebook {
         }
     }
 
+    supportedActionsEvent(event) {
+        // Special elements
+        if (event.code === 'Space') {
+            this.setLineNoteTypeEvent(event, '#', 'header1');
+            this.setLineNoteTypeEvent(event, '##', 'header2');
+            this.setLineNoteTypeEvent(event, '###', 'header3');
+            
+            this.setLineNoteTypeEvent(event, '-', 'bullet-list', 'expand-line', `<ul><li></li></ul>`);
+            this.setLineNoteTypeEvent(event, '1.', 'number-list', 'expand-line', `<ol><li></li></ol>`);
+            this.setLineNoteTypeEvent(event, '-[]', 'selection-list', 'expand-line', `<ul><li><input type="checkbox">Select list not yet implemented</li></ul>`);
+            
+            this.setLineNoteTypeEvent(event, 'img', 'image', 'new-line', `<img src="https://via.placeholder.com/350x200" alt="test">`);
+            
+            
+            this.setLineNoteTypeEvent(event, '&gt;', 'quote', 'new-line', `Quote not yet implemented`); // >
+            this.setLineNoteTypeEvent(event, '&gt;&gt;&gt;', 'code', 'expand-line', `Code not yet implemented`, false); // >>>
+        }
+    }
+
+    focusEvent(event) {
+        const lineNode = event.target;
+
+        if (lineNode.dataset.plainData) {
+            lineNode.innerHTML = lineNode.dataset.plainData;
+        }
+        else {
+            lineNode.innerHTML = '';
+        }
+        
+    }
+    unfocusEvent(event) {
+        const lineNode = event.target;
+        lineNode.dataset.plainData = lineNode.innerHTML;
+        // lineNode.innerHTML = this.render(lineNode);
+        this.render(lineNode);
+    }
+
+    render(element) {
+        const elementValue = element.innerHTML;
+
+        // Math
+        if (element.dataset.mathSupport === 'true') {
+            let mathSplit = elementValue.split(/(\$[^\$]*\$)/);
+            let mathSplitLength = mathSplit.length;
+    
+            element.innerHTML = '';
+            for(let i = 0; i < mathSplitLength; ++i) {
+                let currentMathSplitItem = mathSplit[i];
+                const currentItemLength = currentMathSplitItem.length;
+                
+                // Check if it is math
+                if (currentMathSplitItem.charAt(0) === '$' && currentMathSplitItem.charAt(currentItemLength-1) === '$') {
+                    // Strip dollars
+                    currentMathSplitItem = currentMathSplitItem.split('$').join('');
+    
+                    console.log(currentMathSplitItem);
+                    // Generate math
+                    MathJax.texReset();
+                    let options = MathJax.getMetricsFor(element);
+                    options.display = false;
+    
+                    // MathJax.tex2chtmlPromise(currentMathSplitItem, options).then(function (node) {
+                    
+                    //     element.innerHTML += node.outerHTML;
+                    
+                    //     MathJax.startup.document.clear();
+                    //     MathJax.startup.document.updateDocument();
+                    // })
+    
+                    element.innerHTML += MathJax.tex2chtml(currentMathSplitItem, options).outerHTML;
+                    MathJax.startup.document.clear();
+                    MathJax.startup.document.updateDocument();
+    
+                }
+                else {
+                    element.innerHTML += `<span>${currentMathSplitItem}</span>`;
+                }
+            }
+        }
+
+        // Quick code block
+        // if () {}
+        
+    }
+
+    renderMath(data) {
+        return MathJax.renderString(data);
+    }
+
     newLineNoteEvent(event) {
         event.preventDefault();
         const lineNote = this.createNewLineNote();
@@ -105,14 +186,19 @@ class Notebook {
         event.target.after(lineNote);
         lineNote.focus();
         lineNote.addEventListener('keydown', this.actionEvent.bind(this));
+
+        // Render events
+        lineNote.addEventListener('focus', this.focusEvent.bind(this));
+        lineNote.addEventListener('focusout', this.unfocusEvent.bind(this));
     }
 
-    setLineNoteTypeEvent(event, typeId, type, enterType='new-line', content='') {
+    setLineNoteTypeEvent(event, typeId, type, enterType='new-line', content='', mathSupport=true) {
         const lineNote = event.target;
         if (lineNote.innerHTML === typeId) {
             event.preventDefault();
             lineNote.dataset.type = type;
             lineNote.dataset.enterType = enterType;
+            lineNote.dataset.mathSupport = mathSupport;
             lineNote.innerHTML = content;
         }
     }
